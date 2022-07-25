c066c627dec5d52e21de7f9794ccda21e1101d25
kodaishiotsuki <0711dr5@gmail.com>
2022-07-25 15:50:49 +0900

    pritter

--- src/resources/views/tests/component-test1.blade.php
+++ src/resources/views/tests/component-test1.blade.php
@@ -1,7 +1,7 @@
 <x-tests.app>
-  <x-slot name="header">header1</x-slot>
-test1
-<x-tests.card title="タイトル" content="本文" :message="$message"/>
-<x-tests.card title="タイトル2" />
-<x-tests.card title="cssを変更したい" class="bg-red-300"/>
+    <x-slot name="header">header1</x-slot>
+    test1
+    <x-tests.card title="タイトル" content="本文" :message="$message" />
+    <x-tests.card title="タイトル2" />
+    <x-tests.card title="cssを変更したい" class="bg-red-300" />
 </x-tests.app>
--- src/resources/views/tests/component-test2.blade.php
+++ src/resources/views/tests/component-test2.blade.php
@@ -1,4 +1,4 @@
 <x-tests.app>
-  <x-slot name="header">header1</x-slot>
-  test2
+    <x-slot name="header">header1</x-slot>
+    test2
 </x-tests.app>
